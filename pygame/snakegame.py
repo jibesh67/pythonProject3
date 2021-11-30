0c5855f14f97e5c1865b4164de8c8f3638b47e56
jibeshsingh <jibeshjs67@gmail.com>
2021-11-30 19:33:30 +0545

    project completed

--- pygame/snakegame.py
+++ pygame/snakegame.py
@@ -1,5 +1,4 @@
 # Simple Snake Game in Python 3 for Beginners
-# By @TokyoEdTech
 
 import turtle
 import time
@@ -13,7 +12,7 @@ high_score = 0
 
 # Set up the screen
 wn = turtle.Screen()
-wn.title("Snake Game by @TokyoEdTech")
+wn.title("Snake Game by jibesh")
 wn.bgcolor("green")
 wn.setup(width=600, height=600)
 wn.tracer(0)  # Turns off the screen updates
